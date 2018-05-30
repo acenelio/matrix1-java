55a3243712aabc06e03385b57c24e192a3691587
Nelio Alves <acenelio@gmail.com>
2018-05-30 16:33:00 -0300

    Initial implementation

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+		
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("Main diagonal:");
+		for (int i=0; i<mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
+		
+		int count = 0;
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println("Negative numbers = " + count);
+		
+		
+		sc.close();
+	}
+}
